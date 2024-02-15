ddb570f98ce1082334fe3aaafc89ea3bd0e22a6b
후디 이야기Hoodie’s Story <shinm93@gmail.com>
2024-02-15 22:17:42 +0900

    페이지 레이아웃 스타일

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,16 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header>
+    <header className="flex justify-between items-center p-4">
       <Link href="/">
-        <h1>Hoodie's Blog</h1>
+        <h1 className="text-3xl font-bold">Hoodie's Blog</h1>
       </Link>
-      <nav>
-        <Link href='/'>home</Link>
-        <Link href='/about'>about</Link>
-        <Link href='/posts'>posts</Link>
-        <Link href='/contact'>contact</Link>
+      <nav className="flex gap-4">
+        <Link href="/">home</Link>
+        <Link href="/about">about</Link>
+        <Link href="/posts">posts</Link>
+        <Link href="/contact">contact</Link>
       </nav>
     </header>
-  )
+  );
 }
