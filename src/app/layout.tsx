2374512b5f7cc6cbd973dbaf7acee635ae465b2e
후디 이야기Hoodie’s Story <shinm93@gmail.com>
2024-02-18 11:00:00 +0900

    - 포스트 데이터 불러오기 - 포스트 섹션 구성하기

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
-import Link from "next/link";
-import "./globals.css";
-import { Open_Sans } from "next/font/google";
-import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import { Open_Sans } from "next/font/google";
+import "./globals.css";
 
 const sans = Open_Sans({ subsets: ["latin"] });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import FeaturedPosts from "@/components/FeaturedPosts";
 import Hero from "@/components/Hero";
 
 export default function HomePage() {
   return (
-    <section>
+    <>
       <Hero />
-    </section>
+      {/* @ts-expect-error Server Component */}
+      <FeaturedPosts />
+    </>
   );
 }
