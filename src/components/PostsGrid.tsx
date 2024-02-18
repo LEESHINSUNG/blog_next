b0280faf1d92657403c68ff3cd96f7bc61ed80e8
후디 이야기Hoodie’s Story <shinm93@gmail.com>
2024-02-18 23:27:43 +0900

    포스트 세분화 시키기 (컴포넌트화)

--- src/components/PostsGrid.tsx
+++ src/components/PostsGrid.tsx
@@ -1,4 +1,5 @@
 import { Post } from "@/service/posts";
+import PostCard from "./PostCard";
 
 type Props = {
   posts: Post[];
@@ -8,7 +9,9 @@ export default function PostsGrid({ posts }: Props) {
   return (
     <ul>
       {posts.map((post) => (
-        <li key={post.path}>{post.title}</li>
+        <li key={post.path}>
+          <PostCard key={post.path} post={post} />
+        </li>
       ))}
     </ul>
   );
