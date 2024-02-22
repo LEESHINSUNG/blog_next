42d8bdb032c66660e37e06a90d3ea6061001469d
후디 이야기Hoodie’s Story <shinm93@gmail.com>
2024-02-22 23:09:22 +0900

    - contact 폼 스타일링 - 배너 컴포넌트 및  스타일링

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,3 +1,81 @@
+"use client";
+
+import { ChangeEvent, FormEvent, useState } from "react";
+import Banner, { BannerData } from "./Banner";
+
+type Form = {
+  from: string;
+  subject: string;
+  message: string;
+};
+
+const INITIAL_FORM_DATA: Form = { from: "", subject: "", message: "" };
+
 export default function ContactForm() {
-  return <div></div>;
+  const [form, setForm] = useState(INITIAL_FORM_DATA);
+  const [banner, setBanner] = useState<BannerData | null>(null);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log({ form });
+    setBanner({ message: "성공했어!", state: "success" });
+    setTimeout(() => {
+      setBanner(null);
+    }, 3000);
+  };
+
+  return (
+    <section className="max-w-md w-full">
+      {banner && <Banner banner={banner} />}
+      <form
+        onSubmit={onSubmit}
+        className="w-full flex flex-col gap-2 my-4 p-4 bg-slate-600 rounded-xl text-white"
+      >
+        <label htmlFor="from" className="font-semibold">
+          Your Email
+        </label>
+        <input
+          id="from"
+          name="from"
+          type="email"
+          required
+          autoFocus
+          value={form.from}
+          onChange={onChange}
+        />
+        <label htmlFor="subject" className="font-semibold">
+          subject
+        </label>
+        <input
+          id="subject"
+          name="subject"
+          type="text"
+          required
+          value={form.subject}
+          onChange={onChange}
+          className="text-black"
+        />
+        <label htmlFor="message" className="font-semibold">
+          Message
+        </label>
+        <textarea
+          rows={10}
+          id="message"
+          name="message"
+          required
+          value={form.message}
+          onChange={onChange}
+          className="text-black"
+        />
+        <button className="bg-yellow-300 text-black font-bold hover:bg-yellow-400">
+          Submit
+        </button>
+      </form>
+    </section>
+  );
 }
